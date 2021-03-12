a14ff814f61f9142cb78bb6cd05ef7b13af6d08c
arcangelo7 <arcangelomas@gmail.com>
2021-03-12 18:59:52 +0100

    TimeOut Error

--- dataset_builder.py
+++ dataset_builder.py
@@ -19,62 +19,89 @@ class DatasetBuilder(object):
                 journal_data["message"]["items"].append(item)
         self.data = journal_data
 
-    def get_all_references_from_journal(self):
-        journal_data_json = self.data
-        journal_data_items = journal_data_json["message"]["items"]
-        all_references = list()
-        for item in journal_data_items:
-            references = requests.get(url = f'https://w3id.org/oc/index/coci/api/v1/references/{item["DOI"]}?format=json')
-            references_json = references.json()
-            all_references.append(references_json)
-        return all_references
+    def _manage_citations(self, graphset, item, citing_entity, your_orcid):
+        try:
+            references = requests.get(url = f'https://w3id.org/oc/index/coci/api/v1/references/{item["DOI"]}?format=json').json()
+            for reference in references:
+                reference_id = graphset.add_id(your_orcid)
+                reference_id.create_doi(reference["cited"])
+                # print(reference["cited"])
+                reference_br = graphset.add_br(your_orcid)
+                reference_br.has_identifier(reference_id)
+                # Citation
+                reference_ci = graphset.add_ci(your_orcid)
+                reference_ci.has_citing_entity(citing_entity)
+                reference_ci.has_cited_entity(reference_br)
+                reference_ci.has_citation_creation_date(reference["creation"])
+                reference_ci.has_citation_time_span(reference["timespan"])
+        except json.decoder.JSONDecodeError: 
+            print("\n\njson.decoder.JSONDecodeError")
+            print(item["DOI"] + "\n\n")
     
-    def update_graph(self, your_orcid):
+    def create_graph_from_crossref_data(self, your_orcid):
         journal_graphset = GraphSet("https://arcangelo7.github.io/time_agnostic/")
         journal_data_items = self.data["message"]["items"]
         journal_br = journal_graphset.add_br(your_orcid)
         journal_br.create_journal()
-        journal_item = next((item for item in journal_data_items if item["type"] == "journal"), None)
+        journal_item = next(item for item in journal_data_items if item["type"] == "journal")
+        journal_id = journal_graphset.add_id(your_orcid)
+        journal_id.create_issn(journal_item["ISSN"][0])
+        journal_br.has_identifier(journal_id)
         journal_br.has_title(journal_item["title"][0])
+        journal_re = journal_graphset.add_re(your_orcid)
+        journal_re.create_print_embodiment()
+        journal_re.create_digital_embodiment()
+        journal_re.has_url(URIRef(journal_item["URL"]))
+        journal_br.has_format(journal_re)
         for item in journal_data_items:
-            # AgentRole
-            author_ar = journal_graphset.add_ar(your_orcid)
-            author_ar.create_author()
+            # Identifier
+            item_id = journal_graphset.add_id(your_orcid)
+            item_id.create_doi(item["DOI"])
+            # BibliographicResource
+            if item["type"] != "journal":
+                item_br = journal_graphset.add_br(your_orcid)  
+                item_br.create_journal_article()
+                item_br.has_identifier(item_id)
+                item_br.has_title(item["title"][0])
+                if "subtitle" in item:
+                    item_br.has_subtitle(item["subtitle"][0])
+                # ResourceEmbodiment
+                item_re = journal_graphset.add_re(your_orcid)
+                if "published-online" in item:
+                    pub_date = item["published-online"]["date-parts"][0][0]
+                    item_re.create_digital_embodiment()
+                if "published-print" in item:
+                    pub_date = item["published-print"]["date-parts"][0][0]
+                    item_re.create_print_embodiment()
+                item_re.has_media_type(URIRef("https://www.iana.org/assignments/media-types/" + item["link"][0]["content-type"]))
+                if "page" in item:
+                    item_re.has_starting_page(item["page"].split("-")[0])
+                    item_re.has_ending_page(item["page"].split("-")[1])
+                item_re.has_url(URIRef(item["URL"]))
+                iso_date_string = create_date([pub_date])
+                item_br.has_pub_date(iso_date_string)
+                item_br.is_part_of(journal_br)
+                item_br.has_format(item_re)
             # ResponsibleAgent
             if "author" in item:
                 for author in item["author"]:
-                    journal_ra = journal_graphset.add_ra(your_orcid)
-                    author_ar.is_held_by(journal_ra)
+                    item_ra = journal_graphset.add_ra(your_orcid)
+                    if "ORCID" in author:
+                        author_id = journal_graphset.add_id(your_orcid)
+                        author_id.create_orcid(author["ORCID"])
+                        item_ra.has_identifier(author_id)
                     if "given" in author:
-                        journal_ra.has_given_name(author["given"])
+                        item_ra.has_given_name(author["given"])
                     if "family" in author:
-                        journal_ra.has_family_name(author["family"])
+                        item_ra.has_family_name(author["family"])
                     if "given" in author and "family" in author:
-                        journal_ra.has_name(author["given"] + " " + author["family"])
-            # BibliographicResource
-            if item["type"] != "journal":
-                journal_br = journal_graphset.add_br(your_orcid)  
-                journal_br.create_journal_article()
-                journal_br.has_title(item["title"][0])
-                if "subtitle" in item:
-                    journal_br.has_subtitle(item["subtitle"][0])
-                # ResourceEmbodiment
-                journal_re = journal_graphset.add_re(your_orcid)
-                if "published-online" in item:
-                    pub_date = item["published-online"]["date-parts"][0][0]
-                    journal_re.create_digital_embodiment()
-                if "published-print" in item:
-                    pub_date = item["published-print"]["date-parts"][0][0]
-                    journal_re.create_print_embodiment()
-                journal_re.has_media_type(URIRef("https://www.iana.org/assignments/media-types/" + item["link"][0]["content-type"]))
-                if "page" in item:
-                    journal_re.has_starting_page(item["page"].split("-")[0])
-                    journal_re.has_ending_page(item["page"].split("-")[1])
-                journal_re.has_url(URIRef(item["link"][0]["URL"]))
-                iso_date_string = create_date([pub_date])
-                journal_br.has_pub_date(iso_date_string)
-                journal_br.is_part_of(journal_br)
-                journal_br.has_format(journal_re)
+                        item_ra.has_name(author["given"] + " " + author["family"])
+                    # AgentRole
+                    author_ar = journal_graphset.add_ar(your_orcid)
+                    author_ar.create_author()
+                    author_ar.is_held_by(item_ra)
+                    item_br.has_contributor(author_ar)
+            self._manage_citations(journal_graphset, item, item_br, your_orcid)
         journal_graphset.commit_changes()
         return journal_graphset
 
--- scientometrics.py
+++ scientometrics.py
@@ -4,7 +4,7 @@ from oc_ocdm.graph import GraphSet
 
 scientometrics = DatasetBuilder("1588-2861", "arcangelo.massari@studio.unibo.it")
 # scientometrics.dump_data('data/scientometrics.json')
-scientometrics_graphset = scientometrics.update_graph("https://orcid.org/0000-0002-8420-0696")
+scientometrics_graphset = scientometrics.create_graph_from_crossref_data("https://orcid.org/0000-0002-8420-0696")
 scientometrics.dump_dataset(scientometrics_graphset, "data/graph.json")
 
 
