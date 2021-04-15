f8b88271add37b3cef1df0f0aad7cca96ca2e88a
Arcangelo <info@arcangelomassari.com>
2021-04-15 09:42:22 +0200

    sparql query and output

--- KGEditor/app.py
+++ KGEditor/app.py
@@ -1,23 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from SPARQLWrapper import SPARQLWrapper, JSON, RDFXML
+from SPARQLWrapper.SPARQLExceptions import QueryBadFormed
 from rdflib import Graph
 import json
 
 app = Flask(__name__)
 
 sparql = SPARQLWrapper("http://localhost:9999/bigdata/sparql")
-triples_query = """
-    SELECT ?s ?p ?o
-    WHERE {?s ?p ?o}
-    LIMIT 10
-"""
-sparql.setQuery(triples_query)
-sparql.setReturnFormat(JSON)
-triples_result = sparql.query().convert()
+results = {
+    "results": list()
+}
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("home.jinja2", triples_result=triples_result)
+    if request.method == "POST":
+        try:
+            query = request.form.get("sparqlQuery")
+            sparql.setQuery(query)
+            sparql.setReturnFormat(JSON)
+            results["results"] = sparql.query().convert()["results"]["bindings"]
+            return redirect(url_for("home"))
+        except QueryBadFormed:
+            return redirect(url_for("home"))
+    return render_template("home.jinja2", results=results["results"])
 
 if __name__ == "__main__":
     app.run(debug=True)
